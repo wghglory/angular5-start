d75faf37950991dae4c3cba35a7f7f3d4078bc9e
Guanghui CN1079011 <guanghuiw@guanghuiw-m01.vmware.com>
2017-12-08 11:08:10 +0800

    pipe symbolDisplay in angular 5

--- src/app/shared/convert-to-spaces.pipe.ts
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -4,8 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'convertToSpaces'
 })
 export class ConvertToSpacesPipe implements PipeTransform {
-
-  transform(value: string, character: string): string {
+  transform(value: string, character: string, symbolDisplay: string): string {
+    // symbolDisplay reference: https://github.com/angular/angular/blob/master/packages/common/test/pipes/number_pipe_spec.ts
+    // symbolDisplay is any of "code, symbol, symbol-narrow"
     return value.replace(character, ' ');
   }
 }
